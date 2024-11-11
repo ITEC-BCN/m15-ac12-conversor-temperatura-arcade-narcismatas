7325c4371d01901a18b64bbac9c21c013495b75d
Narcis Matas <146827232+narcismatas@users.noreply.github.com>
2024-11-11 17:21:32 +0100

    Finished

--- main.py
+++ main.py
@@ -1,78 +1,66 @@
 @namespace
 class SpriteKind:
-    button = SpriteKind.create()
-    monkey_type = SpriteKind.create()
+    user = SpriteKind.create()
+    option = SpriteKind.create()
 def CtoF():
     global opcio_usuari
     opcio_usuari = game.ask_for_number("Enter temp in C", 4)
-    return opcio_usuari * (5 / 9) + 32
+    temp = Math.round_with_precision(opcio_usuari * (5 / 9) + 32, 2)
+    game.splash("" + str(opcio_usuari) + " C = " + ("" + str(temp)) + " F")
+
+def on_on_overlap(sprite, otherSprite):
+    global chosen_level2
+    otherSprite.start_effect(effects.star_field, 1000)
+    if otherSprite == forest_a:
+        player.say_text("Celsius a Farenheit", 100, False)
+        if controller.A.is_pressed():
+            chosen_level2 = 1
+            effects.clear_particles(otherSprite)
+            player.set_position(90, 90)
+            CtoF()
+    elif otherSprite == forest_b:
+        player.say_text("Farenheit a Celsius", 100, False)
+        if controller.A.is_pressed():
+            chosen_level2 = 1
+            effects.clear_particles(otherSprite)
+            player.set_position(90, 90)
+            FtoC()
+sprites.on_overlap(SpriteKind.user, SpriteKind.option, on_on_overlap)
+
 def FtoC():
     global opcio_usuari
     opcio_usuari = game.ask_for_number("Enter temp in F", 4)
-    return (opcio_usuari - 32) * (5 / 9)
-def salutacio(opcio: number):
-    CtoF()
-    game.show_long_text("hola", DialogLayout.CENTER)
-    if opcio == 1:
-        game.show_long_text("hola", DialogLayout.TOP)
-    elif opcio == 2:
-        game.show_long_text("adéu", DialogLayout.TOP)
-    else:
-        game.show_long_text("Opció no vàlida", DialogLayout.BOTTOM)
+    temp2 = Math.round_with_precision((opcio_usuari - 32) * (5 / 9), 2)
+    game.splash("" + str(opcio_usuari) + " C = " + ("" + str(temp2)) + " C")
 def choose_level():
-    global fire_plane2, forest_a, forest_b, setting_level
-    chosen_level = 0
-    tiles.set_tilemap(tilemap("""
-        nivel1
+    global forest_a, forest_b
+    scene.set_background_image(assets.image("""
+        Background
     """))
-    if setting_level == 1 and chosen_level == 0:
-        game.splash("Benvingut a la calculadorra de temperatura")
-        game.splash("Escull si passar de C a F", "o de F a C")
-        game.show_long_text("Mou el personatje amb el cursor", DialogLayout.BOTTOM)
-        forest_a = sprites.create(assets.image("""
-            OptA
-        """), SpriteKind.button)
-        forest_b = sprites.create(assets.image("""
-            OptB
-        """), SpriteKind.button)
-        forest_a.set_position(30, 70)
-        forest_b.set_position(130, 70)
-        fire_plane2.set_position(85, 70)
-        fire_plane2.set_bounce_on_wall(True)
-        fire_plane2.set_stay_in_screen(True)
-        controller.move_sprite(fire_plane2)
-    if chosen_level > 0:
-        setting_level = 0
-        sprites.destroy(forest_a)
-        sprites.destroy(forest_b)
-
-
-def on_on_overlap2(sprite, otherSprite):
-    global chosen_level
-    if setting_level == 1:
-        otherSprite.start_effect(effects.halo, 4000)
-        if otherSprite == forest_a:
-            fire_plane2.say_text("Celsius a Farenhein", 10, False)
-            if controller.A.is_pressed():
-                chosen_level = 1
-                effects.clear_particles(otherSprite)
-                CtoF()
-        elif otherSprite == forest_b:
-            fire_plane2.say_text("Farenhein a Celsius", 10, False)
-            if controller.A.is_pressed():
-                chosen_level = 1
-                effects.clear_particles(otherSprite)
-                FtoC()
-sprites.on_overlap(SpriteKind.player, SpriteKind.button, on_on_overlap2)
-
+    game.splash("Benvingut a la calculadora", "de temperatura")
+    game.splash("Escull si passar", "de C a F o de F a C")
+    game.show_long_text("Mou el personatje amb el cursor", DialogLayout.BOTTOM)
+    forest_a = sprites.create(assets.image("""
+        OptA
+    """), SpriteKind.option)
+    forest_b = sprites.create(assets.image("""
+        OptB
+    """), SpriteKind.option)
+    forest_a.set_position(40, 90)
+    forest_b.set_position(120, 90)
+    player.set_position(80, 90)
+    player.set_bounce_on_wall(True)
+    player.set_stay_in_screen(True)
+    controller.move_sprite(player)
+chosen_level2 = 0
 forest_b: Sprite = None
 forest_a: Sprite = None
-fire_plane2: Sprite = None
-chosen_level = 0
+player: Sprite = None
 opcio_usuari = 0
-setting_level = 0
-fire_plane2 = sprites.create(assets.image("""
-        Cross
-    """), SpriteKind.player)
+chosen_level = 0
+player = sprites.create(assets.image("""
+    Cross
+"""), SpriteKind.user)
+
 setting_level = 1
 choose_level()
--- main.ts
+++ main.ts
@@ -1,98 +1,57 @@
 namespace SpriteKind {
-    export const button = SpriteKind.create()
-    export const monkey_type = SpriteKind.create()
+    export const user = SpriteKind.create()
+    export const option = SpriteKind.create()
 }
-
-function CtoF() {
-    
+function CtoF () {
     opcio_usuari = game.askForNumber("Enter temp in C", 4)
-    return opcio_usuari * (5 / 9) + 32
+    let temp = Math.roundWithPrecision(opcio_usuari * (5 / 9) + 32, 2)
+game.splash("" + opcio_usuari + " C = " + ("" + temp) + " F")
 }
-
-function FtoC(): number {
-    
-    opcio_usuari = game.askForNumber("Enter temp in F", 4)
-    return (opcio_usuari - 32) * (5 / 9)
-}
-
-function salutacio(opcio: number) {
-    CtoF()
-    game.showLongText("hola", DialogLayout.Center)
-    if (opcio == 1) {
-        game.showLongText("hola", DialogLayout.Top)
-    } else if (opcio == 2) {
-        game.showLongText("adéu", DialogLayout.Top)
-    } else {
-        game.showLongText("Opció no vàlida", DialogLayout.Bottom)
-    }
-    
-}
-
-function choose_level() {
-    
-    let chosen_level = 0
-    tiles.setTilemap(tilemap`
-        nivel1
-    `)
-    if (setting_level == 1 && chosen_level == 0) {
-        game.splash("Benvingut a la calculadorra de temperatura")
-        game.splash("Escull si passar de C a F", "o de F a C")
-        game.showLongText("Mou el personatje amb el cursor", DialogLayout.Bottom)
-        forest_a = sprites.create(assets.image`
-            OptA
-        `, SpriteKind.button)
-        forest_b = sprites.create(assets.image`
-            OptB
-        `, SpriteKind.button)
-        forest_a.setPosition(30, 70)
-        forest_b.setPosition(130, 70)
-        fire_plane2.setPosition(85, 70)
-        fire_plane2.setBounceOnWall(true)
-        fire_plane2.setStayInScreen(true)
-        controller.moveSprite(fire_plane2)
-    }
-    
-    if (chosen_level > 0) {
-        setting_level = 0
-        sprites.destroy(forest_a)
-        sprites.destroy(forest_b)
-    }
-    
-}
-
-sprites.onOverlap(SpriteKind.Player, SpriteKind.button, function on_on_overlap2(sprite: Sprite, otherSprite: Sprite) {
-    
-    if (setting_level == 1) {
-        otherSprite.startEffect(effects.halo, 4000)
-        if (otherSprite == forest_a) {
-            fire_plane2.sayText("Celsius a Farenhein", 10, false)
-            if (controller.A.isPressed()) {
-                chosen_level = 1
-                effects.clearParticles(otherSprite)
-                CtoF()
-            }
-            
-        } else if (otherSprite == forest_b) {
-            fire_plane2.sayText("Farenhein a Celsius", 10, false)
-            if (controller.A.isPressed()) {
-                chosen_level = 1
-                effects.clearParticles(otherSprite)
-                FtoC()
-            }
-            
+sprites.onOverlap(SpriteKind.user, SpriteKind.option, function (sprite, otherSprite) {
+    otherSprite.startEffect(effects.starField, 1000)
+    if (otherSprite == forest_a) {
+        player.sayText("Celsius a Farenheit", 100, false)
+        if (controller.A.isPressed()) {
+            chosen_level2 = 1
+            effects.clearParticles(otherSprite)
+            player.setPosition(90, 90)
+            CtoF()
         }
-        
+    } else if (otherSprite == forest_b) {
+        player.sayText("Farenheit a Celsius", 100, false)
+        if (controller.A.isPressed()) {
+            chosen_level2 = 1
+            effects.clearParticles(otherSprite)
+            player.setPosition(90, 90)
+            FtoC()
+        }
     }
-    
 })
-let forest_b : Sprite = null
-let forest_a : Sprite = null
-let fire_plane2 : Sprite = null
+function FtoC () {
+    opcio_usuari = game.askForNumber("Enter temp in F", 4)
+    let temp2 = Math.roundWithPrecision((opcio_usuari - 32) * (5 / 9), 2)
+game.splash("" + opcio_usuari + " C = " + ("" + temp2) + " C")
+}
+function choose_level () {
+    scene.setBackgroundImage(assets.image`Background`)
+    game.splash("Benvingut a la calculadora", "de temperatura")
+    game.splash("Escull si passar", "de C a F o de F a C")
+    game.showLongText("Mou el personatje amb el cursor", DialogLayout.Bottom)
+    forest_a = sprites.create(assets.image`OptA`, SpriteKind.option)
+    forest_b = sprites.create(assets.image`OptB`, SpriteKind.option)
+    forest_a.setPosition(40, 90)
+    forest_b.setPosition(120, 90)
+    player.setPosition(80, 90)
+    player.setBounceOnWall(true)
+    player.setStayInScreen(true)
+    controller.moveSprite(player)
+}
+let chosen_level2 = 0
+let forest_b: Sprite = null
+let forest_a: Sprite = null
+let player: Sprite = null
 let chosen_level = 0
 let opcio_usuari = 0
-let setting_level = 0
-fire_plane2 = sprites.create(assets.image`
-        Cross
-    `, SpriteKind.Player)
-setting_level = 1
+player = sprites.create(assets.image`Cross`, SpriteKind.user)
+let setting_level = 1
 choose_level()
